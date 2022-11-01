78e990d24296b2e364f8314ba27adf0b8d676ee2
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-11-01 10:28:48 -0500

    commit tree 24df0d84008a8058fb48e0bc9ecf33912eb755e2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity){
+    if(activity == null){
+        activity = "roller-skate"
+    }
+        return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("bathe my dog"))
+console.log(saturdayFun(null))
+
+function mondayWork(work){
+    if(work == null){
+        work = "go to the office"
+    }
+        return `This Monday, I will ${work}.`
+}
+console.log(mondayWork(null))
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(special){
+    if (special == null){
+        special = "nice"
+    }
+        return `You are ${special}`
+}
